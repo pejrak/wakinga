0bb83268ffefb7821efc1b4d924a1cd1afd06723
nol <nol@mindbeads.com>
2011-01-22 16:44:10 +0100

    UI changes, preparation for alpha live.
    Signed-off-by: nol <nol@mindbeads.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
+
+  def readable_text_area(form, method, options = {})
+    form.text_area(method, "cols" => 40, "rows" => 5)
+  end
+  
 end

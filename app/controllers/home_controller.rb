9c88e824533eaac013d462ea1f4c4a18f127a063
nol <spontain@gmail.com>
2011-02-04 19:21:28 +0100

    incremental change
    Signed-off-by:  <spontain@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,19 +2,6 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
   def index
     @interests = current_user.interests.limit(30)
-    @userratingonbeads = Bead.find_by_sql ["SELECT bp.bead_id as id, bs.title, sum(ps.rating) as rating_sum
-    FROM beads_posts bp
-    INNER JOIN beads bs ON bs.id = bp.bead_id
-    INNER JOIN posts ps ON ps.id = bp.post_id
-    WHERE ps.user_id = ?
-    GROUP by bead_id
-    ORDER by rating_sum DESC
-    LIMIT 5", current_user.id]
-    cu_ratingcounter = 0
-    current_user.posts.each do |sum|
-      cu_ratingcounter += sum.rating
-    end
-    @userrating = cu_ratingcounter
   end
 
 end

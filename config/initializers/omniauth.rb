5308f8a24794fb72e1b8b2210ab3dc56c0d6d675
Pelo Zemina <spontain@gmail.com>
2011-06-19 00:51:14 +0200

    added line to facebook auth for ssl omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'R8FAVGQiGBnItWtrMMvvw', '1ioDqnX6LxgTz5RGHzdjE74X6L3ADcqFLgatnAwv7E4'
-  provider :facebook, '210400255664247', '4db5afe3862e69e5ba2ff6ba235c7c83'
+  provider :facebook, '210400255664247', '4db5afe3862e69e5ba2ff6ba235c7c83', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end

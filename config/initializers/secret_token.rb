cf1173a39db91bc5c55aaf830bca820d6e01be00
nol@mind11 <spontain@gmail.com>
2011-06-28 19:44:11 -0400

    changing secret token file in order to support devise change
    Signed-off-by: nol@mind11 <spontain@gmail.com>

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mindbase::Application.config.secret_token = 'e63399007804b5fb90bee7c2567f25cecaee13eabc1d37086f0ba2050408652898eaef1f2e0917267b177f0f5847024229d1fbf82c2a323360ba8be476634e14'
+Mindbase::Application.config.secret_token = 'e63399007804b5fb90bee7c2567f25cecaee13eabc1d37086f0ba2050408652898eaef1f2e0917267b177f0f5847024229d1fbf82c2a323360ba8be476634e17'

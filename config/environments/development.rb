7a99ccfbbf01400517b728206e3e7a30c01e4f1e
Pelo Zemina <spontain@gmail.com>
2011-07-15 21:44:33 +0200

    fixed gmail sending email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Mindbase::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "smtp.gmail.com", :port => 587, :domain => "wakinga.com", :user_name => "spontain", :password => "aiolus1234",:authentication => "plain",:enable_starttls_auto => true }
+  config.action_mailer.smtp_settings = { :address => "smtp.gmail.com", :port => 587, :domain => "wakinga.com", :user_name => "wakingamail", :password => "5carred5hitle55",:authentication => "plain",:enable_starttls_auto => true }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log

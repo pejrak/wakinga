7ee31b4032403aefecee561ccc2672ed1c7b7cc1
nol@mind11 <spontain@gmail.com>
2011-07-15 13:13:40 -0400

    fixed connection with gmail from dev environment, the emails are still timing out
    Signed-off-by: nol@mind11 <spontain@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,18 +17,8 @@ Mindbase::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  
-	#GMAIL mailer setup
-	config.action_mailer.delivery_method = :smtp
-	config.action_mailer.smtp_settings = { 
-	:address => 'smtp.gmail.com', 
-	:port => '587', 
-	:domain => 'wakinga.com', 
-	:user_name => 'wakingamail', 
-	:password => '5carred5hitle55', 
-	:authentication => 'plain', 
-	:enable_starttls_auto => true 
-	}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "smtp.gmail.com", :port => 587, :domain => "wakinga.com", :user_name => "spontain", :password => "aiolus1234",:authentication => "plain",:enable_starttls_auto => true }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log

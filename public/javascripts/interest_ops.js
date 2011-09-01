cd134a1b7335f2eef1d104aae788ece33d0808c2
spontain@gmail.com <spontain@gmail.com>
2011-09-01 21:29:07 +0200

    fixes for IE version X Signed-off-by: spontain@gmail.com <spontain@gmail.com>

--- public/javascripts/interest_ops.js
+++ public/javascripts/interest_ops.js
@@ -22,7 +22,7 @@ function updatePosts() {
 //  } else {
 //    var after = "0";
 //  }
-  $.getScript("/posts.js?interest_id=" + interest_id + "&after=" + after + "&full_refresh=false" + "&previous_visit_record=" + previous_visit);
+  $.getScript("/posts.js?interest_id=" + interest_id + "&after=" + after + "&full_refresh=false&previous_visit_record=" + previous_visit);
   setTimeout(updatePosts, 10000);
 
 
@@ -44,7 +44,7 @@ function prepPosts() {
   var interest_id = $("#interest").attr("data-id");
   var previous_visit = $(".dynamic#post_content").attr("data-time");
   $("#flash_notice, #flash_error").fadeOut(10000);
-  $.getScript("/posts.js?interest_id=" + interest_id + "&full_refresh=true" + "&previous_visit_record=" + previous_visit);
+  $.getScript("/posts.js?interest_id=" + interest_id + "&full_refresh=true&previous_visit_record=" + previous_visit);
 }
 
 

c0ea98001f7652c12b7e82187cf3839fdc330da7
spontain@gmail.com <spontain@gmail.com>
2011-09-18 00:51:08 +0200

    additional fix to comply with pg validation
    Signed-off-by: spontain@gmail.com <spontain@gmail.com>

--- app/models/noun.rb
+++ app/models/noun.rb
@@ -2,7 +2,7 @@ class Noun < ActiveRecord::Base
 
   def self.search(search)
 	if search
-	  find(:all, :limit => 30, :conditions => ['UPPER(nouns.title) LIKE UPPER(?) AND nouns.b_active <> ?', "%#{search}%", 1], :order => 'nouns.title')
+	  find(:all, :limit => 30, :conditions => ['UPPER(nouns.title) LIKE UPPER(?) AND nouns.b_active = ?', "%#{search}%", false], :order => 'nouns.title')
 	else
 	  find(:all, :limit => 10, :order => 'nouns.title' )
 	end

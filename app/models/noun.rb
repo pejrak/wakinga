81160d914a5c6f064d7e889bbd5474763e1eb0d8
spontain@gmail.com <spontain@gmail.com>
2011-09-18 00:52:25 +0200

    commit on top to resolve push issues Signed-off-by: spontain@gmail.com <spontain@gmail.com>

--- app/models/noun.rb
+++ app/models/noun.rb
@@ -1,6 +1,7 @@
 class Noun < ActiveRecord::Base
 
   def self.search(search)
+  #searching for nouns
 	if search
 	  find(:all, :limit => 30, :conditions => ['UPPER(nouns.title) LIKE UPPER(?) AND nouns.b_active = ?', "%#{search}%", false], :order => 'nouns.title')
 	else

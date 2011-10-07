715c50b4e7792428b311a620745eee3caaf7b4b3
nol@mind11 <spontain@gmail.com>
2011-10-07 13:24:35 +0200

    added refresh messages improvement
    Signed-off-by: nol@mind11 <spontain@gmail.com>

--- public/javascripts/interest_ops.js
+++ public/javascripts/interest_ops.js
@@ -31,7 +31,13 @@ function updatePosts(c) {
   var interest_id = $("#interest").attr("data-id");
   var previous_visit = $(".dynamic#postcontent").attr("data-time");
   var after = $(".post:first").attr("data-time");
+  //if message refresh is visible, remove it, so that it does not display during timed refresh
+  if ($("#messagerefresh").length > 0) {
+    $("#messagerefresh").remove();
+  }
+  
   $.getScript("/posts.js?interest_id=" + interest_id + "&after=" + after + "&full_refresh=false&previous_visit_record=" + previous_visit);
+  //reload message operations scripts, because they get disabled by running multiple layers of scripts before
   $.getScript("/javascripts/message_ops.js");
   //iterating timeout count
   if (c == undefined) {
@@ -43,13 +49,14 @@ function updatePosts(c) {
   }
   else {
     clearTimeout(t);
+    $("#interest").after("<div id='messagerefresh'><a href='#' data-remote='true' onClick='updatePosts(0)'><img src='/images/message_refresh.png' border = 0 />refresh messages</a></div>")
   }
 }
 
 jQuery.ajaxSetup({
   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 })
-
+//submittal of post/messages form, making ajax request for the new messages
 jQuery.fn.submitWithAjax = function() {
   this.submit(function() {
     $.post(this.action, $(this).serialize(), null, "script");
@@ -61,9 +68,6 @@ jQuery.fn.submitWithAjax = function() {
 function prepPosts() {
   var interest_id = $("#interest").attr("data-id");
   var previous_visit = $(".dynamic#postcontent").attr("data-time");
-  $("#flash_notice, #flash_error").fadeOut(10000);
+  $("#flash_notice, #flash_error").fadeOut(6000);
   $.getScript("/posts.js?interest_id=" + interest_id + "&full_refresh=true&previous_visit_record=" + previous_visit);
 }
-
-
-

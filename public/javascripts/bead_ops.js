21bbb52e282ac2e9c54acd67a431684ef126acdd
nol@mind11 <spontain@gmail.com>
2011-10-10 04:11:13 +0200

    MOD changes
    Signed-off-by: nol@mind11 <spontain@gmail.com>

--- public/javascripts/bead_ops.js
+++ public/javascripts/bead_ops.js
@@ -1,5 +1,4 @@
   $(document).ready(function() {
-
     //bead hover and activate effects
     $(".bead_point").live({
       mouseenter: function () {
@@ -12,19 +11,21 @@
       },
       click: function() {
       var identificator = $(this).attr("data-id");
-        $(this).after("<div class='bead_group' id='beadpoint"+identificator+"' data-id="+identificator+"></div>");
-        $("#beadpoint"+identificator).html("<p><img src='/images/loader.gif'/> Loading...</p>");
         //$(this).parent().css("background-color","#d1ffc0");
         var parent_ids = $(this).parent().data("beadpoints");
         var active_array = $.merge([identificator], parent_ids);
         //$(this).data("beadpoints", [identificator]);
         if ($(this).parent().attr("class")=="bead_point_container") {
           var initializer = "true";
+          if ($(".bead_group#beadpoint"+identificator).length==0) {
+            $(this).after("<div class='bead_group' id='beadpoint"+identificator+"' data-id="+identificator+"></div>");
+          }
+          $("#beadpoint"+identificator).html("<p><img src='/images/loader.gif'/> Loading...</p>");
+          $.getScript("/bead_point_load.js?bead_id="+identificator+"&beads_in_path="+active_array+"&initialize="+initializer);
         }
         else {
           var initializer = "false";
         }
-        $.getScript("/bead_point_load.js?bead_id="+identificator+"&beads_in_path="+active_array+"&initialize="+initializer);
       }
     });
     

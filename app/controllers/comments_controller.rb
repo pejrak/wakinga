6ed81bd3fd4a369c9fa331862814adee9709ab81
nol <spontain@gmail.com>
2012-02-16 23:02:44 +0100

    cool off for comment
    Signed-off-by: nol <spontain@gmail.com>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,18 @@ before_filter :authenticate_user!
 	def create
 		
 		@post = Post.find(params[:post_id])
-		@comment = @post.comments.new(params[:comment])
-    @comment.user = current_user
+    if (Time.now - current_user.comments.last.created_at) > 5
+      @comment = @post.comments.new(params[:comment])
+      @comment.user = current_user
       if @comment.save
         respond_to do | format |
           flash[:notice] = "Comment added."
           format.js {render :layout => false}
         end
       end
+    else
+      flash[:notice] = "Cooling off."
+    end
 	end
 	def destroy 
 		@post = Post.find(params[:post_id]) 

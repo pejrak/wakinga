64d88fff7d5facf66e2b6e8bb5b168c79332e53e
nol <spontain@gmail.com>
2012-04-20 20:32:21 +0200

    rho fix45

--- config/initializers/rhoconnect.rb
+++ config/initializers/rhoconnect.rb
@@ -3,7 +3,7 @@ require 'rhoconnect-rb'
 Rhoconnect.configure do |config|
   #config.app_endpoint = "http://127.0.0.1"
   config.token = "1d2006ac42c46c885227d1525d074131909eefb2caf8a897c3ecd391c6770b5767d8f4e673f1cb132a62b2ce01edf3cf53b12bd3e8dc296c7499fcb22eca95ba"
-  config.uri = "http://ec2-23-21-16-53.compute-1.amazonaws.com:9292"
+  config.uri = "http://ec2-23-21-16-53.compute-1.amazonaws.com"
   config.authenticate = lambda { |credentials|
     User.authenticate(credentials['login'], credentials['password'])
   }

0725f1788ada22241c67e4f0fc544f6e901ddfc4
nol <spontain@gmail.com>
2012-04-25 21:28:55 +0200

    fcr1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,16 @@
 class Comment < ActiveRecord::Base
-belongs_to :post
-belongs_to :user
+
+    include Rhoconnect::Resource
+
+  belongs_to :post
+  belongs_to :user
 
   MAX_BODY_LENGTH = 300
   validates :body, :presence => true, :length => { :minimum => 1, :maximum => MAX_BODY_LENGTH }
+
+  def partition
+    lambda { self.user.username }
+  end
 
   def new?(selected_user)
 #    loaded_post_memorization = post.memorizations.where(:memorable => true, :user_id => selected_user.id).first

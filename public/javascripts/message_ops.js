3e764645705df856d240f7179321c65c680bad4c
nol@mind11 <spontain@gmail.com>
2012-06-01 19:12:11 +0200

    changing preview operations

--- public/javascripts/message_ops.js
+++ public/javascripts/message_ops.js
@@ -1,7 +1,7 @@
   $(document).ready(function() {
 
     //message hover and activate effects
-    $(".post").live({
+    $(".posty").live({
       mouseover: function () {
         var identificator = $(this).attr("id");
         $("#post_operators_"+identificator).show();
@@ -18,6 +18,32 @@
       }
     });
 
+$("body").delegate(".post","mouseout",function(){
+  $(this).css("background-color","transparent");
+});
+
+$("body").delegate(".post","mouseover",function(){
+  $(this).css("background-color","#d1ffc0");
+});
+
+
+$("body").delegate(".post","click",function(){
+  $(".post").animate({
+    width:"44%"
+  }, 500);
+  $("p",".post").animate({
+    fontSize:"12px"
+  }, 500);
+  
+  $(this).animate({
+    width:"94%"
+  }, 500);
+  $("p",this).animate({
+    fontSize:"18px"
+  }, 500);
+  var identificator = $(this).attr("id");
+  $.getScript("/posts/" + identificator + "/activate.js");
+});
 
 //fade out for notices after actions
   $("#flash_notice, #flash_error, .flash_dynamic").fadeOut(10000);

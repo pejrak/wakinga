5a94b0d30586636cbb5738d2317f2ded53a6415f
nol@mind11 <spontain@gmail.com>
2012-06-07 17:26:35 +0200

    UI changes to stream items v2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,12 +13,12 @@ class Comment < ActiveRecord::Base
 #  end
 
   def new?(selected_user)
-#    loaded_post_memorization = post.memorizations.where(:memorable => true, :user_id => selected_user.id).first
-#    if loaded_post_memorization
-#      (updated_at > loaded_post_memorization.updated_at)? true : false
-#    else
+    loaded_post_memorization = post.memorizations.where(:memorable => true, :user_id => selected_user.id).first
+    if loaded_post_memorization
+      (updated_at > loaded_post_memorization.updated_at)? true : false
+    else
     (updated_at > selected_user.last_sign_in_at)? true : false
-#    end
+    end
   end
 
 end
